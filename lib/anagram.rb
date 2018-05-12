fdb357a0c34ec03fd25d9d28910f5aa7beab2486
Richard Cho <richardcho100@gmail.com>
2018-05-12 03:21:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
 
   def match(word_list)
     #binding.pry
-    if word_list.detect {|word| word == @word} == nil
+    if word_list.detect {|word| word.split("").sort == @word.split("").sort} == nil
       []
     else
       word_list.detect {|word| word.split("").sort == @word.split("").sort}
