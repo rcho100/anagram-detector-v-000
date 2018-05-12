a08ff5999189f64fadcfd0c32802854c4a2e68f2
Richard Cho <richardcho100@gmail.com>
2018-05-12 03:32:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    #binding.pry
+    if word_list.select {|word| word.split("").sort == @word.split("").sort} == nil
+      []
+    else
+      word_list.select {|word| word.split("").sort == @word.split("").sort}
+      #another way to do this is to use .chars instead of .split("")
+    end
+  end
+end
