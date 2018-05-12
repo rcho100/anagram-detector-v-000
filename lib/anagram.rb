b117f3301a72ce7c87adea02f722ccaa082bf5b6
Richard Cho <richardcho100@gmail.com>
2018-05-12 03:20:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    binding.pry
+    if word_list.detect {|word| word == @word} == nil
+      []
+    else
+      word_list.detect {|word| word == @word.split}
+    end
+  end
+end
